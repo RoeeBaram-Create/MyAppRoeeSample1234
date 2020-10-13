0271084bb40b5308b9ca088e854e69b57eeda24f
David Mordechai <david.mordechai79@gmail.com>
2020-10-13 12:52:54 +0300

    added logger to applicaton services

--- FunProject.Application/ActivityLogModule/Services/ActivityLogService.cs
+++ FunProject.Application/ActivityLogModule/Services/ActivityLogService.cs
@@ -1,7 +1,9 @@
 ﻿using FunProject.Application.ActivityLogModule.Dtos;
 using FunProject.Application.ActivityLogModule.Services.Interfaces;
 using FunProject.Application.Data.ActivityLogs.Query;
+using FunProject.Domain.Logger;
 using FunProject.Domain.Mapper;
+using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
@@ -11,15 +13,30 @@ namespace FunProject.Application.ActivityLogModule.Services
     {
         private readonly IAllActivityLogs _allActivityLogs;
         private readonly IMapperAdapter _mapperAdapter;
+        private readonly ILoggerAdapter<ActivityLogService> _logger;
 
-        public ActivityLogService(IAllActivityLogs allActivityLogs,
-            IMapperAdapter mapperAdapter)
+        public ActivityLogService(
+            IAllActivityLogs allActivityLogs,
+            IMapperAdapter mapperAdapter, 
+            ILoggerAdapter<ActivityLogService> logger)
         {
             _allActivityLogs = allActivityLogs;
             _mapperAdapter = mapperAdapter;
+            _logger = logger;
         }
 
-        public async Task<IList<ActivityLogDto>> GetAllActivityLogs() =>
-            _mapperAdapter.Map<IList<ActivityLogDto>>(await _allActivityLogs.Get());
+        public async Task<IList<ActivityLogDto>> GetAllActivityLogs()
+        {
+            _logger.LogInformation("Method GetAllActivityLogs was hit...");
+            try
+            {
+                return _mapperAdapter.Map<IList<ActivityLogDto>>(await _allActivityLogs.Get());
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Method GetAllActivityLogs failed");
+                throw;
+            }
+        }
     }
 }
--- FunProject.Application/CustomersModule/Services/CustomersService.cs
+++ FunProject.Application/CustomersModule/Services/CustomersService.cs
@@ -3,7 +3,9 @@ using FunProject.Application.CustomersModule.Services.Interfaces;
 using FunProject.Application.Data.Customers.Command;
 using FunProject.Application.Data.Customers.Query;
 using FunProject.Domain.Entities;
+using FunProject.Domain.Logger;
 using FunProject.Domain.Mapper;
+using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
@@ -16,37 +18,81 @@ namespace FunProject.Application.CustomersModule.Services
         private readonly ICreateCustomer _createCustomer;
         private readonly IDeleteCustomer _deleteCustomer;
         private readonly IMapperAdapter _mapperAdapter;
+        private readonly ILoggerAdapter<CustomersService> _logger;
 
         public CustomersService(
             ICustomerById customerById,
             IAllCustomers allCustomers, 
             ICreateCustomer createCustomer,
             IDeleteCustomer deleteCustomer,
-            IMapperAdapter mapperAdapter)
+            IMapperAdapter mapperAdapter,
+            ILoggerAdapter<CustomersService> logger)
         {
             _customerById = customerById;
             _getAllCustomers = allCustomers;
             _createCustomer = createCustomer;
             _deleteCustomer = deleteCustomer;
             _mapperAdapter = mapperAdapter;
+            _logger = logger;
         }
 
-        public async Task CreateCustomer(CustomerDto customer) => 
-            await _createCustomer.Create(_mapperAdapter.Map<Customer>(customer));
+        public async Task CreateCustomer(CustomerDto customer)
+        {
+            _logger.LogInformation("Method CreateCustomer was hit...");
+            try
+            {
+                await _createCustomer.Create(_mapperAdapter.Map<Customer>(customer));
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Method CreateCustomer failed");
+                throw;
+            }
+        }
 
-        public async Task<CustomerDto> GetCustomer(int? id) => 
-            _mapperAdapter.Map<CustomerDto>(await _customerById.Get(id));
+        public async Task<CustomerDto> GetCustomer(int? id)
+        {
+            _logger.LogInformation("Method GetCustomer was hit...");
+            try
+            {
+                return _mapperAdapter.Map<CustomerDto>(await _customerById.Get(id));
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Method GetCustomer failed");
+                throw;
+            }
+        }
 
-        public async Task<IList<CustomerDto>> GetAllCustomers() => 
-            _mapperAdapter.Map<IList<CustomerDto>>(await _getAllCustomers.Get());
+        public async Task<IList<CustomerDto>> GetAllCustomers()
+        {
+            _logger.LogInformation("Method GetAllCustomers was hit...");
+            try
+            {
+                return _mapperAdapter.Map<IList<CustomerDto>>(await _getAllCustomers.Get());
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Method GetAllCustomers failed");
+                throw;
+            }
+        }
 
         public async Task DeleteCustomer(int? id)
         {
-            var customer = await _customerById.Get(id);
-            if (customer != null)
+            _logger.LogInformation("Method DeleteCustomer was hit...");
+            try
             {
-                await _deleteCustomer.Delete(customer);
-                
+                var customer = await _customerById.Get(id);
+                if (customer != null)
+                {
+                    await _deleteCustomer.Delete(customer);
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Mehtod DeleteCustomer failed");
+                throw;
             }
         }
     }
